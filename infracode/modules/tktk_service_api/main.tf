6a9ba16dc6766fea0edd21ed65125d9a266247a9
Erik Steringer <erik.steringer@nccgroup.com>
2022-01-12 09:25:26 -0800

    Making API externally reachable

--- infracode/modules/tktk_service_api/main.tf
+++ infracode/modules/tktk_service_api/main.tf
@@ -159,7 +159,7 @@ resource "aws_security_group" "api_sg" {
     protocol  = "tcp"
     from_port = 443
     to_port   = 443
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
