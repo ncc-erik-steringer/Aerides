2996a0612bd97b4986d79c7977351b9f3a648e82
Erik Steringer <erik.steringer@nccgroup.com>
2022-01-12 09:58:44 -0800

    updated ingress rules

--- infracode/modules/tktk_service_api/main.tf
+++ infracode/modules/tktk_service_api/main.tf
@@ -157,8 +157,32 @@ resource "aws_security_group" "api_sg" {
   ingress {
     description = "Incoming TLS"
     protocol  = "tcp"
+    from_port = 80
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Incoming TLS"
+    protocol  = "tcp"
     from_port = 443
     to_port   = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Incoming TLS"
+    protocol  = "tcp"
+    from_port = 445
+    to_port   = 445
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Incoming TLS"
+    protocol  = "tcp"
+    from_port = 3389
+    to_port   = 3389
     cidr_blocks = ["0.0.0.0/0"]
   }
 
